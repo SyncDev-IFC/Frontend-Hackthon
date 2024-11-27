79178094284d882ad02a0073716d91b66cbd56cc
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-27 20:25:17 -0300

    Iniciando Projeto

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+
+// Components
+import App from './App.vue'
+import router from './router'
+
+// Composables
+import { createApp } from 'vue'
+
+const app = createApp(App);
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
+
+registerPlugins(app)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Bem-vindo à página inicial!</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
