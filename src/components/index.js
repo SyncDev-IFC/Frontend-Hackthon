77c296168f686998333ad0523d4e9c1cee54b74c
= <=>
2024-11-29 15:23:37 -0300

    FIX: erro de nome de arquivo e arrumando menu

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import loginComponent from "./auth/loginComponent.vue"
 import CardAluno from "./CardAluno.vue"
+import CardHeader from "./CardHeader.vue"
 export {
     loginComponent,
     CardAluno,
+    CardHeader
 }
