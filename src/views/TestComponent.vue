300ec327e77a9ece80125cc8d3becc1d1af9e1c1
= <=>
2024-11-29 14:54:42 -0300

    FEAT: Criação do Card aluno

--- src/views/TestComponent.vue
+++ src/views/TestComponent.vue
@@ -1,11 +1,27 @@
 <script setup>
-import {HeaderView} from '@/components';
+import {CardAluno} from '@/components';
 
+import { ref } from 'vue'
+
+const alunos = ref([
+  { nome: 'João Vitor Gomes', email: 'joao.vitor.gomes@gmail.com', },
+  { nome: 'Lucas Liebl', email: 'lucas.liebl@gmail.com', },
+  { nome: 'Gabriel Lima Pereira', email: 'gabriel.lima@gmail.com',},
+  { nome: 'Eduardo Monta', email: 'eduardo.monta@gmail.com', },
+  { nome: 'Heloisa Albano', email: 'heloisa.albano@gmail.com', },
+  { nome: 'Kethelyn Talia', email: 'kethelyn.talia@gmail.com', }
+])
 </script>
 
 
 <template>
-<HeaderView/>
+<CardAluno 
+v-for="aluno in alunos" :key="aluno"
+:nome="aluno.nome"
+:email="aluno.email"
+:foto="aluno.foto"
+/>
+
 </template>
 <style scoped>
 
