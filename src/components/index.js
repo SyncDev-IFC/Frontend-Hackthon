6adffb9f29e94f749472aab206876070a3d6d1fb
= <=>
2024-11-29 14:54:42 -0300

    FEAT: Criação do Card aluno

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import loginComponent from "./auth/loginComponent.vue"
+import CardAluno from "./CardAluno.vue"
 import CardHeader from "./ComponentsPrincipais/CardHeader.vue"
 import CardAluno from "./CardAluno.vue"
 export {
