bd73a27d85e5c72398dd8f60a1d156b71320a2d0
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-30 01:32:46 -0300

    feat: Criação da pagina conselho

---
+++ src/components/ConselhoPage/HeaderLogado.vue
@@ -0,0 +1,57 @@
+<script setup>
+import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
+</script>
+
+<template>
+    <div class="header">
+        <ArrowLeft class="arrow-left" />
+
+        <div class="right-content">
+            <!-- <button class="btn-black-rounded">Sair do conselho</button> -->
+            <img src="https://i.ibb.co/R9LNsWn/user.png" alt="User avatar" class="user-avatar">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 10px;
+    gap: 70em;
+}
+
+.arrow-left {
+    font-size: 24px;
+    cursor: pointer;
+}
+
+.right-content {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+}
+
+.btn-black-rounded {
+    background-color: #000;
+    color: #fff;
+    border: none;
+    border-radius: 20px;
+    padding: 8px 16px;
+    cursor: pointer;
+    font-size: 14px;
+    transition: background-color 0.3s ease;
+}
+
+.btn-black-rounded:hover {
+    background-color: #333;
+}
+
+.user-avatar {
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+    border-radius: 50%;
+}
+</style>
---
+++ src/views/ConselhoEntrar/ConselhoEntrarView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { NotConselho, HaveConselho } from '@/components';
+import { useConselhoStore } from '@/stores/conselho';
+
+// Acessando a store de conselho
+const conselhoStore = useConselhoStore();
+</script>
+
+<template>
+    <div>
+        <HaveConselho v-if="conselhoStore.conselho && conselhoStore.conselho.status === 'aberto'" />
+        <NotConselho v-else />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
